6b1e8f5efa4f28dc67bae374a3a127dd0bfbaa72
Samandar Bakhodirov <samandarbaxodirov11@gmail.com>
2025-06-04 19:55:46 +0300

    status-route fix

--- src/app/api/status/[id]/route.ts
+++ src/app/api/status/[id]/route.ts
@@ -1,20 +1,14 @@
-// pages/api/templates/[id]/status.ts
-
-import {prisma} from "@/lib/prisma"; // или твоя ORM
+import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function PUT(req:Request, { params }: { params: { id: string } }) {
-  const {id} = await params
-
-  
-
-  const {status} = await req.json();
+export async function PUT(req: Request, { params }: { params: { id: string } }) {
+  const { id } = params;
+  const { status } = await req.json();
 
   const updated = await prisma.template.update({
-      where: { id: Number(id) },
-      data: { status },
+    where: { id: Number(id) },
+    data: { status },
   });
 
-  return NextResponse.json({data: updated})
-
-  }
+  return NextResponse.json({ data: updated });
+}
