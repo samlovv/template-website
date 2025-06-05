7378274d303bdeba5f973980187e9e15ff38ec54
Samandar Bakhodirov <samandarbaxodirov11@gmail.com>
2025-06-05 12:31:54 +0300

    template id route fix

--- src/app/api/templates/[id]/route.ts
+++ src/app/api/templates/[id]/route.ts
@@ -1,18 +1,24 @@
-import {prisma} from '@/lib/prisma'
-import { NextResponse } from 'next/server'
+import { prisma } from '@/lib/prisma';
+import { NextResponse } from 'next/server';
 
+export async function GET(req: Request, context: { params: { id: string } }) {
+  const { id } = context.params;
 
-export async function GET(req:Request, { params }: { params: { id: string } }) {
-  const {id} = await params
-  const template = await prisma.template.findUnique({ where: { id: Number(id) },
-      include: {
-      user:{
-        select:{
+  const template = await prisma.template.findUnique({
+    where: { id: Number(id) },
+    include: {
+      user: {
+        select: {
           nickname: true,
           image: true,
-        }
-      }
-    }})
-  if (!template) return NextResponse.json({ error: 'Not found' }, { status: 404 })
-  return NextResponse.json(template)
+        },
+      },
+    },
+  });
+
+  if (!template) {
+    return NextResponse.json({ error: 'Not found' }, { status: 404 });
+  }
+
+  return NextResponse.json(template);
 }
